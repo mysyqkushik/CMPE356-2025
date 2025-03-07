824c50b5d77d18f081ee5b9b59bac4b08b324b31
srahzzzz <sarah.nauman@stu.khas.edu.tr>
2025-03-07 17:19:08 +0300

    further additions in navbar

---
+++ src/pages/NavBarPages/BorrowBookInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./BorrowBookInfo.css";
+
+const BorrowBookInfo = () => {
+  return (
+    <div className="borrowbook-info-containerb35">
+      <h1 className="titleb35">How to Borrow a Book</h1>
+
+      <p className="descriptionb35">
+        Borrowing a book is simple! To get started, you need to create an account. You can do this by entering your email address and creating a secure password. Create an account, enter your email and password, or log in if you already have an account. Once logged in, you can borrow books directly from your dashboard.
+      </p>
+
+      <p className="descriptionb35">
+        You'll be taken to your personal dashboard where you can browse through available books. Simply select the book you'd like to borrow, and you're all set!
+      </p>
+
+      <div className="image-containerbb35">
+        <img
+          src="./paulrudd.png"
+          alt="Paul Rudd as Josh reading some Friedrich Nietzsche by the pool in Clueless"
+          className="borrowbook-imageb35"
+        />
+        <p className="image-captionb35">
+          Paul Rudd as Josh reading some Friedrich Nietzsche by the pool in <i>Clueless</i>.
+        </p>
+      </div>
+
+
+      <a href="/CustomerLogin" className="go-to-borrow-buttonb35">
+        Go to Borrow
+      </a>
+
+      <a href="/HomePage" className="return-home-buttonb35">
+        Return to Home Page
+      </a>
+    </div>
+  );
+};
+
+export default BorrowBookInfo;
---
+++ src/pages/NavBarPages/ReturnBookInfo.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './ReturnBookInfo.css'; // Link to the CSS file
+
+function ReturnBookInfo() {
+  return (
+    <div className="returnbook-info-container67">
+      <h1 className="title67">How to Return a Book</h1>
+      <p className="description67">
+        Returning a book is easy! After finishing your book, simply log in to your account, go to your
+        dashboard, and select the "Return Book" option. You can return the book at the library or arrange
+        for a pickup. Don't forget to make sure that the book is in good condition before returning it.
+      </p>
+      <p className="description67">
+        You need to create an account by adding your email and password, or log in if you already have an account. 
+        Once you're logged in, you can see the books you have borrowed, and simply select the one you'd like to return.
+      </p>
+
+      <div className="image-container67">
+        <img 
+          src="./juliastiles.jpg" 
+          alt="Person returning a book" 
+          className="returnbook-image67" 
+        />
+        <p className="image-caption67">
+          Julia Stiles as Kat reading The Bell Jar by Sylvia Path in <i>10 Things I Hate About You</i>
+        </p>
+      </div>
+
+
+
+      <a href="/CustomerLogin" className="go-to-return-buttonb67">Go to Return</a>
+      <a href="/HomePage" className="return-home-buttonb67">Return to Homepage</a>
+    </div>
+  );
+}
+
+export default ReturnBookInfo;
